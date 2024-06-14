d118999633a96fee65a3dadcac4799c6d3d70237
Cande <111658579+Cnsgnicole@users.noreply.github.com>
2024-06-13 22:49:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express=require('express');
 const app=express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 const usurouter = require('./routes/histo_medico')
 
@@ -10,6 +16,6 @@ app.get("/",(request,response)=>{
     response.send("Inicio pagina de mascotas")
 })
 
-app.listen(3000,function() { 
-    console.log("Historial Medico");
+app.listen(3000,function() { //tiene que estar debajo de todo
+    console.log("La app funciona");
 })
